eee5eee69f4d406153cd8cca29e040e897e40424
henrichard <glosskraken@gmail.com>
2022-11-16 11:28:32 +0500

    feat: add feedback

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,11 +1,9 @@
-import { v4 as uuidv4 } from 'uuid';
 import { createContext, useState, useEffect } from 'react';
-// import FeedbackData from '../data/FeedbackData';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [isLoading, setIsloading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const [feedback, setFeedback] = useState([]);
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
@@ -22,13 +20,21 @@ export const FeedbackProvider = ({ children }) => {
             'http://localhost:5000/feedback?_sort=id&_order=desc',
         );
         const data = await response.json();
+
         setFeedback(data);
-        setIsloading(false);
+        setIsLoading(false);
     };
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback]);
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('http://localhost:5000/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newFeedback),
+        });
+        const data = await response.json();
+        setFeedback([data, ...feedback]);
     };
 
     const deleteFeedback = (id) => {
