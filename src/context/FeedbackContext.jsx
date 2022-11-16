f142c7ff7ffa027820aa563eb9f4afd8b04b3297
henrichard <glosskraken@gmail.com>
2022-11-16 10:11:23 +0500

    feat: add json-server mock backend, concurrently to run client & server, fetch data from json-server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,15 +1,30 @@
 import { v4 as uuidv4 } from 'uuid';
-import { createContext, useState } from 'react';
-import FeedbackData from '../data/FeedbackData';
+import { createContext, useState, useEffect } from 'react';
+// import FeedbackData from '../data/FeedbackData';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState(FeedbackData);
+    const [isLoading, setIsloading] = useState(true);
+    const [feedback, setFeedback] = useState([]);
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false,
     });
+
+    useEffect(() => {
+        fetchFeedback();
+    }, []);
+
+    //* Fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch(
+            'http://localhost:5000/feedback?_sort=id&_order=desc',
+        );
+        const data = await response.json();
+        setFeedback(data);
+        setIsloading(false);
+    };
 
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4();
@@ -42,6 +57,7 @@ export const FeedbackProvider = ({ children }) => {
             value={{
                 feedback,
                 feedbackEdit,
+                isLoading,
                 deleteFeedback,
                 addFeedback,
                 editFeedback,
