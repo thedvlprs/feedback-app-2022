e250add83dd94bddc8cb7d8ccaafe61f4a9b18e1
henrichard <glosskraken@gmail.com>
2022-11-16 11:46:35 +0500

    feat: update & delete from json-server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -37,8 +37,11 @@ export const FeedbackProvider = ({ children }) => {
         setFeedback([data, ...feedback]);
     };
 
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
+            await fetch(`http://localhost:5000/feedback/${id}`, {
+                method: 'DELETE',
+            });
             setFeedback(feedback.filter((item) => item.id !== id));
         }
     };
@@ -50,10 +53,20 @@ export const FeedbackProvider = ({ children }) => {
         });
     };
 
-    const updateFeedback = (id, updItem) => {
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updItem),
+        });
+
+        const data = await response.json();
+
         setFeedback(
             feedback.map((item) =>
-                item.id === id ? { ...item, ...updItem } : item,
+                item.id === id ? { ...item, ...data } : item,
             ),
         );
     };
